5868572451a10fb0526012d37634c8d1ea61d4da
Alan McKenna <alanmckenna72@gmail.com>
2020-10-07 20:18:55 +0100

    adding BlogPost component

---
+++ src/components/BlogPost.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import { colors } from '../constants'
+
+const styles = {
+    container: {
+    },
+    title: {
+        width: '100%',
+        paddingTop: 30,
+        paddingBottom: 30,
+        textAlign: 'center'
+    },
+    contentContainer: {
+        backgroundColor: colors.beige,
+        width: '60%',
+        paddingLeft: '20%',
+        paddingRight: '20%',
+        paddingTop: 30,
+        paddingBottom: 30,
+        textAlign: 'center'
+    },
+    content: {
+        gridColumn: '2',
+        backgroundColor: 'inherit',
+        textAlign: 'left'
+    },
+}
+
+function BlogPost({ title, child }) {
+    
+  return (
+    <div className="page" style={styles.container}>
+        <div className="page-title" style={styles.title}>
+            {title}
+        </div>
+        <div className="page-content-container" style={styles.contentContainer}>
+            <div style={styles.contentContainer}>
+                {child}
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default BlogPost;
