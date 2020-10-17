29e122fdd382a168519a517d6d71032e68552f14
Alan McKenna <alanmckenna72@gmail.com>
2020-10-17 13:47:42 +0100

    refactoring PostEditor to be agnostic with saving data. This will help for reuse with creation and editing of posts. Pushing up functionality to CreatePost

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect, useRef } from 'react';
+import Loader from 'react-loader-spinner'
 
 import Page from './Page'
 import PostEditor from './PostEditor'
 import CustomAlert from './CustomAlert'
 
-import { fontSize, colors } from '../styles'
+import AuthService from '../services/auth.service.js'
+import BlogPostService from '../services/blogPost.service.js'
+
+import { fontSize, colors, button } from '../styles'
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
 
 const styles = {
   title: {
@@ -13,6 +18,12 @@ const styles = {
     border: 'none',
     width: '90%',
     textAlign: 'center'
+  },
+  button: button,
+  submitContainer: {
+    paddingBottom: 30,
+    backgroundColor: colors.beige,
+    textAlign: 'center',
   }
 }
 
@@ -31,22 +42,61 @@ function SaveAlert({ isSaveSuccessful, showAlert, setShowAlert }) {
       }
     </>
   )
+}
 
+function SubmitButton({ isWaiting, handleSave }) {
+
+  return (
+    <div>
+      {isWaiting
+        ?   <Loader type="BallTriangle" color="#00BFFF" height={80} width={80} />
+        
+        :   <div onClick={() => handleSave()} style={styles.submitContainer}>
+                <input type="submit" value="Save" style={styles.button}/>
+            </div>
+        }
+    </div>
+  )
 }
 
 function CreatePost() {
-  const [title, setTitle] = useState("")
-  const [isSaveSuccessful, setIsSaveSuccessful] = useState(true);
-  const [showAlert, setShowAlert] = useState(false)
-  const firstUpdate = useRef(true);
+    const [title, setTitle] = useState("")
+    const [isSaveSuccessful, setIsSaveSuccessful] = useState(true);
+    const [showAlert, setShowAlert] = useState(false)
+    const [isWaiting, setIsWaiting] = useState(false);
+    const [editorInstance, setEditorInstance] = useState("");
+    const firstUpdate = useRef(true);
 
     useEffect(() => {
       if (!firstUpdate.current) {
+        console.log(`isSaveSuccessful=${isSaveSuccessful}`)
         setShowAlert(true)
       }
       firstUpdate.current = false;
     }, [firstUpdate, isSaveSuccessful]);
 
+    useEffect(() => {
+        console.log(`title=${title}`)
+    }, [title]);
+    useEffect(() => {
+        console.log(`isSaveSuccessful=${isSaveSuccessful}`)
+    }, [isSaveSuccessful]);
+
+    async function handleSave() {
+      setIsWaiting(true)
+      const savedData = await editorInstance.save();
+      const post = {
+          title: title,
+          author: AuthService.getCurrentUser(),
+          comments: [],
+          tags: [],
+          blocks: savedData.blocks,
+      }
+      const isSaveSuccessful = await BlogPostService.save(post)
+        console.log(`isSaveSuccessful=${isSaveSuccessful}`)
+        setIsSaveSuccessful(isSaveSuccessful)
+      setIsWaiting(false)
+    }
 
     return (
       <div className="contact">
@@ -61,8 +111,9 @@ function CreatePost() {
                 onChange={(event) => setTitle(event.target.value)}
               />
             }
-            child={<PostEditor title={title} setIsSaveSuccessful={setIsSaveSuccessful}/>}
+            child={<PostEditor setEditorInstance={setEditorInstance}/>}
         />
+        <SubmitButton isWaiting={isWaiting} handleSave={handleSave}/>
       </div>
     );
   }
--- src/components/PostEditor.js
+++ src/components/PostEditor.js
@@ -1,40 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import EditorJs from "react-editor-js";
-import Loader from 'react-loader-spinner'
 
-import BlogPostService from '../services/blogPost.service.js'
-
-import { button } from "../styles";
 import { EDITOR_JS_TOOLS } from "../editorConstants";
 
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
-
-const styles = {
-    submit: button,
-    row: {
-        paddingTop: 10,
-    }
-}
-
-
-function PostEditor({ title, setIsSaveSuccessful }) {
-    const [ editorInstance, setEditorInstance ] = useState("");
-    const [ isWaiting, setIsWaiting ] = useState(false);
-
-    async function handleSave() {
-        setIsWaiting(true)
-        const savedData = await editorInstance.save();
-        const post = {
-            title: title,
-            tags: [],
-            blocks: savedData.blocks,
-        }
-        const isSaveSuccessful = await BlogPostService.save(post)
-        setIsSaveSuccessful(isSaveSuccessful)
-        setIsWaiting(false)
-    }
-
-
+function PostEditor({ setEditorInstance }) {
     return (
         <>
         <EditorJs
@@ -44,16 +13,6 @@ function PostEditor({ title, setIsSaveSuccessful }) {
             blocks: [],
             }}
         />
-        {isWaiting
-        ?   <Loader type="BallTriangle" color="#00BFFF" height={80} width={80} />
-        
-        :   <div className="row"
-                onClick={() => handleSave()}
-                style={styles.row}
-            >
-                <input type="submit" value="Save" style={styles.submit}/>
-            </div>
-        }
         </>
     );
 }
