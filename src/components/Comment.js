0b9182f689c97cc19477edc7dd7bc6a194ec6643
Alan McKenna <alanmckenna72@gmail.com>
2020-10-24 14:32:11 +0100

    addign styling to comments

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useMediaQuery } from 'react-responsive'
+import moment from 'moment'
 
 import { colors, fontSize, device } from '../styles'
 
@@ -9,22 +10,37 @@ function Comment({ author, date, content }) {
 
     const styles = {
         container: {
-        },
-        content: {
-        },
-        meta: {
+            border: '1px solid black',
+            borderRadius: '5px',
+            padding: '10px',
+            marginTop:  '30px',
+            marginBottom:  '30px',
         },
         metaContainer: {
+            display: 'grid',
+            gridTemplateColumns: '50% 50%',
+            gridTemplateRows: 'minmax(50px, 100%)',
+            width: '100%',
+        },
+        author: {
+            gridColumn: '1',
+        },
+        date: {
+            gridColumn: '2',
+            textAlign: 'right',
+        },
+        content: {
+            marginBottom:  '20px',
         },
     }
 
     return (
         <div className="comment" style={styles.container}>
-            <div className="comment-content">{content}</div>
-            <div className="comment-meta-container">
-                <div className="comment-meta">{author}</div>
-                <div className="comment-meta">{date}</div>
+            <div className="comment-meta-container" style={styles.metaContainer} >
+                <div className="comment-meta" style={styles.author}>{author}</div>
+                <div className="comment-meta" style={styles.date}>{moment(date).format('MMMM DD YYYY')}</div>
             </div>
+            <div className="comment-content" style={styles.content}>{content}</div>
         </div>
     );
 }
